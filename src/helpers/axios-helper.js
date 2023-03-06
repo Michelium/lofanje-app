00bf0017838fbf45592933fee5014a0389e4f954
Michel Hamelink <info@michelhamelink.nl>
2023-03-06 11:30:37 +0100

    changed dev to prod api endpoint

--- src/helpers/axios-helper.js
+++ src/helpers/axios-helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const axiosInstance = axios.create({baseURL: 'https://lofanjedt.nl/api'});
-const axiosInstance = axios.create({ baseURL: "http://192.168.68.126:8000/api" });
+const axiosInstance = axios.create({baseURL: 'https://lofanjedt.nl/api'});
+// const axiosInstance = axios.create({ baseURL: "http://192.168.68.126:8000/api" });
 
 export default axiosInstance;
